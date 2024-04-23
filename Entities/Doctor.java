7da124671490feab75c44c6209dc039a88baf525
Lucas Gasperin <luscagape@gmail.com>
2024-04-23 08:56:38 -0300

    added constructor for Doctor

--- Entities/Doctor.java
+++ Entities/Doctor.java
@@ -6,4 +6,13 @@ public class Doctor {
   private String name;
   private int code;
   ArrayList<Patient> patients;
+
+  public Doctor(String name, int code, ArrayList<Patient> patients){
+    this.name = name;
+    this.code = code;
+    this.patients = patients;
+  }
+
+  
+
 }
